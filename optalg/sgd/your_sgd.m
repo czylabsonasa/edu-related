464ab84d15c0386a998aec50278dccb7ccf813c8
czylabsonasa <czylabson.asa@gmail.com>
2025-03-18 11:25:37 +0100

    sgd+varik

--- optalg/sgd/your_sgd.m
+++ optalg/sgd/your_sgd.m
@@ -1,4 +1,6 @@
-clf;
+clear;clc;
+figure(1);clf(1);
+
 Q=dlmread("pontok200E.txt");
 Q=Q';
 [~,M]=size(Q);
@@ -15,12 +17,15 @@ dLq=@(w,q) 2*(fun(w,q(1),q(2))-q(3))*dfun(w,q(1),q(2));
 L=mkL(Lq,Q,M);
 dL=mkdL(dLq,Q,M);
 
-w0=rand(3,1)-0.5;
-[optloc,optval]=lsqnonlin(L, w0);
+RNG=2222;
+rng(RNG);
 
-a=0.2;
-E=10000;
-B=20;
+w0=2*(rand(3,1)-0.5);
+opt_w=lsqnonlin(L, w0);
+
+a=0.5;
+E=20;
+B=10;
 w=w0;
 history=[L(w)];
 for e=1:E
@@ -37,9 +42,11 @@ for e=1:E
 end
 plot(history)
 hold on
-plot([1,E],[val,val])
-abs(history(end)-optval)
-
+L_opt=L(opt_w);
+plot([1,E+1],[L_opt,L_opt]);
+title(sprintf( ...
+   "sgd\nRNG=%d\na=%.3f B=%d E=%d\nvs.lsqnonlin=%.3f",...
+   RNG,a,B,E,abs(history(end)-L_opt)));
 
 
 
