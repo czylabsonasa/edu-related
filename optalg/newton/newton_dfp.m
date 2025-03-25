18fe59238714a56edf30db920b20f420fbe8a2ab
czylabsonasa <czylabson.asa@gmail.com>
2025-03-25 11:20:52 +0100

    opt

--- optalg/newton/newton_dfp.m
+++ optalg/newton/newton_dfp.m
@@ -6,15 +6,14 @@ function [history,status]=newton_dfp(f,df,x0)
    B=eye(length(x0));
 
    for it=1:33
-      p=B\(-df0);
-      % 122 oldal
+      p=B\(-df0); % vagy p=H*(-df0), 122 oldal környéke
       a=armijo(f,df,p,x0,p,1,0.8,0.8);
       if norm(a*p)<1e-8, status="xtol";break;end
       x1=x0+a*p;
       history=[history,x0];
       df1=df(x1);
       if norm(df1)<1e-5, status="dftol";break;end
-      % itt kiszamoljuk az uj B-t...
+      % itt kiszamoljuk az uj B-t...vagy H-t
    end
    if status=="",status="maxit";end
 end
